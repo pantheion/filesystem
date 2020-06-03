82c0c913b80143a302a3a5c047895633f270e5d6
Pedro Miguel <pedro.aos19@gmail.com>
2020-06-03 23:00:13 +0100

    2

---
+++ Folder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pantheion\Filesystem;
+
+class Folder
+{
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function test()
+    {
+        echo "asaaaaaaaaaaadasd";
+    }
+}
